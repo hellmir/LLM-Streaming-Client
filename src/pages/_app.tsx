d28f4982ceb7e879b976d7c8ce340ea5bbfb1c98
hellmir <hellmir610@gmail.com>
2025-02-10 15:50:59 +0900

    [LSCT-030] refactor: Add models and improve some codes (#25)
    - package.json
    - package-lock.json
    - _app.tsx
    - index.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,14 @@
 import "@/styles/globals.css";
 import 'github-markdown-css/github-markdown.css';
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
     return (
         <>
-            {/* 글로벌 메타데이터 설정 */}
             <Head>
                 <title>생성형 AI 스트리밍 서비스</title>
-                <link rel="icon" href="/ai.png" type="image/png" /> {/* 기존 vite.svg 대신 logo.png */}
+                <link rel="icon" href="/ai.png" type="image/png"/>
             </Head>
             <Component {...pageProps} />
         </>
